6176f57ef7f373eac454bb12a6012f8a4dee4b68
alexander3605 <as6316@ic.ac.uk>
2017-11-07 11:28:57 +0000

    change in data space format

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,25 +1,31 @@
-int execute(u8& mem[RAM_SIZE], int& pc) {
+#include <vector>
+#include "constants.hpp"
+using namespace std;
+int execute(vector <unsigned char> &instructions, unsigned char (&data)[DATA_SIZE], unsigned char (&registers)[32] , int& pc) {
 
 	//get instruction from 4 bytes in mem
-	u32 instruction = mem[pc];
+	unsigned int instr = instructions[pc];
 	for (int x = 1; x < 4; x++){
-		instruction = instruction << 8;
-		instruction += mem[pc + x];
+		instr = instr << 8;
+		instr += instructions[pc + x];
 	}
 
 	//decode instruction and call correct subfunction
 	//isolate opcode
-	unsigned char opcode = instruction >> 26;
+	unsigned char opcode = instr >> 26;
 	//check if R type
 	if (opcode == 0)
 		//execute
-		execute_R(mem, instruction);
+		execute_R(instr, data, registers);
 	//check if J type
 	else if (opcode == 2 || opcode == 3){
 		//execute
-		execute_J(mem, instruction, opcode);
+		execute_J(instr, data, registers, opcode, pc);
 	}
 	//further decode and (eventually) execute
-	else execute_I(mem, instruction, opcode);
+	else execute_I(instr, data, registers, opcode);
 }
+
+
+
 
--- src/functions.hpp
+++ src/functions.hpp
@@ -1,4 +1,4 @@
-int execute(unsigned char (&mem)[RAM_SIZE], unsigned int& pc);
+int execute(vector <unsigned char> &instructions, unsigned char (&data)[DATA_SIZE], unsigned char (&registers)[32] , int& pc);
 void execute_R(unsigned char (&mem)[RAM_SIZE], unsigned int& instruction);
 void execute_J(unsigned char (&mem)[RAM_SIZE], unsigned int& instruction, unsigned char& opcode);
 void execute_I(unsigned char (&mem)[RAM_SIZE], unsigned int& instruction, unsigned char& opcode);
