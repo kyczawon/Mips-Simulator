e7cca086ae1f53b89a75412503953f0c3bbe759b
Your Name <you@example.com>
2017-11-12 17:27:39 +0000

    create one cpp file and decode add instruction

--- src/functions.hpp
+++ src/functions.hpp
@@ -1,8 +1,15 @@
 #include <vector>
 using namespace std;
-int execute(vector <unsigned char> &instructions, unsigned char (&data)[DATA_SIZE], int (&registers)[32] , unsigned int& pc);
-void execute_R(unsigned char (&mem)[RAM_SIZE], unsigned int& instruction);
-void execute_J(unsigned char (&mem)[RAM_SIZE], unsigned int& instruction, unsigned char& opcode);
-void execute_I(unsigned char (&mem)[RAM_SIZE], unsigned int& instruction, unsigned char& opcode);
+void execute(unsigned int instr, unsigned char* data, int (&registers)[32] , unsigned int& pc);
+void execute_R(unsigned int instr, unsigned char* data, int (&registers)[32]);
+void execute_J(unsigned int instr, unsigned char* data, int (&registers)[32], unsigned char& opcode, unsigned int& pc);
+void execute_I(unsigned int instr, unsigned char* data, int (&registers)[32], unsigned char& opcode);
+void decode_fields_R (unsigned int &dest_reg, unsigned int &op1, unsigned int &op2, unsigned int &shift_amt, unsigned int &funct_code, const unsigned int &instr);
+
+//R type instructions
+void add(unsigned int dest_reg, unsigned int op1, unsigned int op2, int (&registers)[32]);
+
+//helper functions
+unsigned int bin_string_to_int(string input);
 
 
